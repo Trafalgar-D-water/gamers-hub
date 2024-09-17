6b025f1dc42fae37aef4a3ab92a9d6dd87448ae1
priyanshu <patnayakpriyanshu06@gmail.com>
2024-09-17 14:21:54 +0530

    friend and adim

--- server/manifest.js
+++ server/manifest.js
@@ -7,7 +7,8 @@ const auth = require("../lib/plugins/auth/index");
 const team = require("../lib/plugins/team/index");
 const profile = require('../lib/plugins/profile/index')
 const socket = require('../lib/plugins/socket/index')
-const friends = require('../lib/plugins/friends/index')
+const friends = require('../lib/plugins/friends/index');
+const hapiAuthorization = require("hapi-authorization");
 module.exports = {
   server: {
     port: process.env.PORT || 3004,
@@ -20,7 +21,8 @@ module.exports = {
       },
       cors: {
         origin: ["*"],  
-        headers: ["Authorization", "Content-Type", "Accept"], 
+        headers: ["Authorization", "Content-Type", "Accept" ,'If-None-Match'] , 
+        additionalHeaders: ['x-requested-with'] ,
         credentials: true,
       },
     },
@@ -54,6 +56,12 @@ module.exports = {
           schemes: ["http"],
         },
       },
+      {
+        plugin : hapiAuthorization,
+        options : {
+          roles :['superAdmin' , 'admin' , 'mod' , 'user']
+        }
+      },
 
       { plugin: auth },
       { plugin: users },
@@ -63,4 +71,21 @@ module.exports = {
       {plugin : friends}
     ],
   },
+  // ext: {
+  //   onPreHandler: (request, h) => {
+  //     const userRole = request.auth.credentials.role; // Ensure this is set in your JWT validation
+
+  //     // Define protected routes
+  //     const protectedRoutes = ['/admin', '/moderator']; // Adjust protected routes as needed
+
+  //     if (protectedRoutes.includes(request.path) && userRole !== 'superAdmin') {
+  //       return h.response({ message: 'Access denied' }).code(403);
+  //     }
+
+  //     return h.continue;
+  //   }
+  // }
 };
+
+
+
